f158262ccdb1e23d395e19d1e4237d6e5b365653
Hasini Wijerathna <hasininwj@gmail.com>
2019-03-24 12:36:34 +0800

    Added footerbar component

---
+++ src/components/FooterBar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class FooterBar extends Component {
+
+  render(){
+    return(
+      <div>
+        Footer
+      </div>
+    );
+  }
+}
+
+export default FooterBar;

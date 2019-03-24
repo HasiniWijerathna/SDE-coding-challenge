95c951684986e95ebd9d5d770baf098ba312b841
Hasini Wijerathna <hasininwj@gmail.com>
2019-03-24 12:37:23 +0800

    Added Headerbar component

---
+++ src/components/HeaderBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HeaderBar extends Component {
+
+  render() {
+
+    return (
+      <div>
+        HeaderBar
+    </div>
+    );
+  }
+}
+
+export default HeaderBar;

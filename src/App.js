463cc21ed55f5c2eaee7a5d15a0541e3e743be34
Hasini Wijerathna <hasininwj@gmail.com>
2019-03-30 19:52:33 +0800

    Added MuiThemeProvider material UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+import HeaderBar from './components/HeaderBar';
+import FooterBar from './components/FooterBar';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import './App.css'
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ffffff',
+      dark: '#ffffff',
+      contrastText: '#ffffff',
+    },
+    text: {
+    
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      borderRadius: 3,
+      border: 0,
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    },
+    secondary: {
+      light: '#4da9b7',
+      main: '#017a87',
+      dark: '#004e5a',
+      contrastText: '#000',
+    },
+  },
+  typography: {
+    fontFamily: "'Lato', sans-serif",
+    textTransform: "none",
+    useNextVariants: true,
+
+
+
+    button: {
+      text: { // Name of the rule
+        color: 'white', // Some CSS
+      },
+      textTransform: "none",
+
+    },
+    multilineColor: {
+      color: 'red'
+    },
+
+  }
+});
+
+
+const App = props => ({
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <HeaderBar />
+        {props.children}
+        <FooterBar />
+      </MuiThemeProvider>
+
     );
   }
-}
+});
 
 export default App;

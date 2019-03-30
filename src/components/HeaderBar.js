edc02fb65c3536f2641ab8cc43a7e613c0cdb613
Hasini Wijerathna <hasininwj@gmail.com>
2019-03-31 01:36:53 +0800

    Header : Added logout button

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
+import history from "../history";
+import { resetSession } from "../services/SessionService";
+import { withStyles } from "@material-ui/core";
+import Button from '@material-ui/core/Button';
 
 class HeaderBar extends Component {
+
+  /**
+   * Sets the authenticated false and navigates to the home page
+   */
+  signOut = () => {
+    resetSession();
+    history.push('/login');
+  }
 
   render() {
 
     return (
       <div>
-        HeaderBar
-    </div>
+        <div children="search-bar-container">
+          <div className="search-bar-signout">
+            <LogoutButton onClick={this.signOut}>
+              Logout
+            </LogoutButton>
+          </div>
+        </div>
+      </div>
     );
   }
 }
+
+
+const LogoutButton = withStyles({
+  root: {
+    background: '#ffffff',
+    borderRadius: 3,
+    "&:hover": {
+      backgroundColor: "#c8d2d2"
+    },
+    border: 0,
+
+    height: 38,
+    width: 80,
+    padding: '0 30px',
+  },
+
+})(Button);
+
 
 export default HeaderBar;

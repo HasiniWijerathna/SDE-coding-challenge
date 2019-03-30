f6b532edfb33835ee0c477412d97d3fe22c70749
Hasini Wijerathna <hasininwj@gmail.com>
2019-03-31 00:37:11 +0800

    Added session service This is responsible for handling the application session status and check for request authenticity

---
+++ src/services/SessionService.js
@@ -0,0 +1,40 @@
+import history from '../history';
+
+/**
+ * Returns current session
+ * @return {object}  Currernt session object
+ */
+const getSession = () => {
+    const session = localStorage.session ? JSON.parse(localStorage.session) : {authenticated: false};
+    if (session) {
+      return session;
+    } else {
+        history.push('login');
+    }
+  };
+
+/**
+ * Update session
+ * @param  {object} session Updated session
+ */
+const setSession = (session) => {
+    localStorage.session = JSON.stringify(session);
+  };
+
+/**
+ * Checks if the current sesion is authenticated
+ * @return {Boolean}  true if current session is authenticated
+ */
+const isAuthenticated = () => `${getSession().authenticated}` === 'true';
+
+/**
+ * Reset current session
+ */
+const resetSession = () => {
+    const session = {
+      authenticated: false,
+    };
+    setSession(session);
+  };
+  
+  export {getSession, setSession, isAuthenticated, resetSession};

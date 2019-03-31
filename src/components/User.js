8f11c61347af165b38482dc2bf4ac1536aacc3ab
Hasini Wijerathna <hasininwj@gmail.com>
2019-03-31 09:41:52 +0800

    Refactor: User component

--- src/components/User.js
+++ src/components/User.js
@@ -10,11 +10,15 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
 import DoneIcon from '@material-ui/icons/Done';
-import DeleteIcon from '@material-ui/icons/Delete';
-import Grid from '@material-ui/core/Grid';
 import CardMedia from '@material-ui/core/CardMedia';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import { withStyles } from "@material-ui/core";
+import Button from '@material-ui/core/Button';
 
 
+//  LOADER IS MISSING!!!!!!!
 /**
  * Representing the user component functionality
  */
@@ -26,7 +30,6 @@ class User extends Component {
     onChangeFirstName = (changeEvent) => {
         const newFirstName = changeEvent.target.value;
         this.props.onChangeFirstName(newFirstName);
-
     }
 
     /**
@@ -40,7 +43,7 @@ class User extends Component {
 
     /**
      * Remnove the selected from the users list
-     * @param  {String} id           The selected user ID
+     * @param  {String} id      The selected user ID
      */
     onRemoveItem = id => {
         this.props.onRemoveItem(id)
@@ -48,7 +51,7 @@ class User extends Component {
 
     /**
     * Update the selected users attributes
-    * @param  {String} id           The selected user ID
+    * @param  {String} id       The selected user ID
     */
     onUpdateItem = (id) => {
         this.props.onUpdateItem(id);
@@ -61,74 +64,69 @@ class User extends Component {
     render() {
         return (
             <div>
-                <ExpansionPanel key={this.props.userData.id}>
-                    <ExpansionPanelSummary expandIcon={<EditIcon />}>
-                       <Grid container spacing={24}>
-                            <Grid item xs={6}>
-                                <Grid container spacing={24}>
-                                    <Grid item xs={4}>
-                                        <CardMedia
-                                            component="img"
-                                            alt="Contemplative Reptile"
-
-                                            height="140"
-                                            src={this.props.userData.avatar}
-                                            title="Contemplative Reptile"
+                <Card >
+                    <CardContent>
+                        <div className="form-user-card-container">
+                            <div className="card-user-avatar">
+                                <CardMedia
+                                    component="img"
+                                    alt="Contemplative Reptile"
+                                    height="100"
+                                    width="100"
+                                    image={this.props.userData.avatar}
+                                    title="Contemplative Reptile"
+                                />
+                            </div>
+                            <div className="card-user-content">
+                                <ExpansionPanel key={this.props.userData.id}>
+                                    <ExpansionPanelSummary expandIcon={<EditIcon />}>
+                                        <div className="card-user-name">
+                                            <Typography className="center-container">
+                                                {this.props.userData.first_name + " " + this.props.userData.last_name}
+                                            </Typography>
+                                        </div>
+                                    </ExpansionPanelSummary>
+                                    <ExpansionPanelDetails>
+                                        <TextField
+                                            id="standard-name"
+                                            label="First Name"
+                                            defaultValue={this.setState.firstName ? this.setState.firstName : this.props.userData.first_name}
+                                            margin="normal"
+                                            onChange={this.onChangeFirstName}
+                                            helperText={this.props.nameError.firstName}
                                         />
-                                    </Grid>
-                                    <Grid item xs={6}>
-                                        <Typography className="center-container">
-                                            {this.props.userData.first_name + " " + this.props.userData.last_name}
-                                        </Typography>
-                                    </Grid>
-                                </Grid>
-                            </Grid>
-                            <Grid item xs={6}>
-                                <Grid container spacing={24}>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                        <Grid container spacing={24}>
-
-                            <Grid item xs={3}>
-                                <TextField
-                                    id="standard-name"
-                                    label="First Name"
-                                    defaultValue={this.setState.firstName ? this.setState.firstName : this.props.userData.first_name}
-                                    margin="normal"
-                                    onChange={this.onChangeFirstName}
-                                    helperText={this.props.nameError.firstName}
-                                />
-                            </Grid>
-                            <Grid item xs={3}>
-                                <TextField
-                                    id="standard-name"
-                                    label="Last Name"
-                                    defaultValue={this.props.userData.last_name}
-                                    margin="normal"
-                                    onChange={this.onChangeLastName}
-                                    helperText={this.props.nameError.lastName}
-                                />
-
-                                <IconButton onClick={() => this.onRemoveItem(this.props.userData.id)} >
-                                    <DeleteIcon />
-                                </IconButton>
-                                <IconButton onClick={() => this.onUpdateItem(this.props.userData.id)} disabled={this.props.nameError.firstName != null || this.props.nameError.lastName != null } >
-                            <DoneIcon />
-                        </IconButton>
-                            </Grid>
-                        </Grid>
-
-                    </ExpansionPanelDetails>
-
-                </ExpansionPanel>
-
+                                        <TextField
+                                            id="standard-name"
+                                            label="Last Name"
+                                            defaultValue={this.props.userData.last_name}
+                                            margin="normal"
+                                            onChange={this.onChangeLastName}
+                                            helperText={this.props.nameError.lastName}
+                                        />
+                                        <IconButton onClick={() => this.onUpdateItem(this.props.userData.id)}
+                                            disabled={this.props.nameError.firstName != null || this.props.nameError.lastName != null}
+                                            style={{ backgroundColor: 'transparent' }} >
+                                            <DoneIcon />
+                                        </IconButton>
+                                    </ExpansionPanelDetails>
+                                </ExpansionPanel>
+                            </div>
+                        </div>
+                    </CardContent>
+                    <CardActions>
+                        <div className="card-user-action-container">
+                            <div className="card-user-action-content"></div>
+                            <div className="card-user-action-button">
+                                <DeleteButton variant="contained" color="primary" onClick={() => this.onRemoveItem(this.props.userData.id)}>
+                                    Delete
+                                </DeleteButton>
+                            </div>
+                        </div>
+                    </CardActions>
+                </Card>
             </div>
         );
     }
-    
 }
 User.propTypes = {
     userData: PropTypes.object.isRequired,
@@ -137,9 +135,22 @@ User.propTypes = {
     onChangeLastName: PropTypes.func.isRequired,
     onUpdateItem: PropTypes.func.isRequired,
     nameError: PropTypes.object.isRequired,
-  };
+};
 
+const DeleteButton = withStyles({
+    root: {
+        background: '#1976D2',
+        borderRadius: 3,
+        "&:hover": {
+            backgroundColor: "#2196F3"
+        },
+        border: 0,
+        color: 'white',
+        height: 38,
+        width: 80,
+        padding: '0 30px',
+    },
+
+})(Button);
 
 export default User;
-
-
